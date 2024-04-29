5cd5e483091d0e82a3585d93653d14d13ed80ff9
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-29 19:20:17 +0100

    init express app & comments

--- networkNode.js
+++ networkNode.js
@@ -1,10 +1,16 @@
+// declare imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const WebSocket = require('ws');
 const path = require('path');
 const {Blockchain,Transaction} = require('./Blockchain');
 
+// parse command line args
 const httpPort = parseInt(process.argv[2] || 3001);
 const p2pPort = parseInt(process.argv[3] || 6001);
-
 const peers = process.argv.slice(4);
+
+// init express app
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));

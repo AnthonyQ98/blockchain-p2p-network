9cef6bd22b2c62f33ffacf6822b2d03a8aadb666
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-29 20:13:28 +0100

    Define blockchain class

--- Blockchain.js
+++ Blockchain.js
@@ -20,4 +20,28 @@ class Block {
     calculateHash() {
         return SHA256(this.timestamp + this.previousHash + JSON.stringify(this.transactions) + this.nonce).toString();
     }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+    } 
+}
+
+class BlockChain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
+        this.pendingTransactions = [];
+        this.miningReward = 100;    
+    }
+
+    createGenesisBlock() {
+        return new Block(Date.parse("2024-04-29"), [], "0");
+    }
+
+    getLatestBlock() {
+        return this.chain(this.chain.length - 1);
+    }
 }

5a46c46d90ce225672c89917acdcf78a0b182d2c
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-29 19:18:10 +0100

    parse commandline args

--- networkNode.js
+++ networkNode.js
@@ -3,3 +3,8 @@ const bodyParser = require('body-parser');
 const WebSocket = require('ws');
 const path = require('path');
 const {Blockchain,Transaction} = require('./Blockchain');
+
+const httpPort = parseInt(process.argv[2] || 3001);
+const p2pPort = parseInt(process.argv[3] || 6001);
+
+const peers = process.argv.slice(4);

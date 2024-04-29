298e4f4fe1a820173e161cbcd4f9cbc25d4b7f34
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-04-29 20:02:08 +0100

    define block class, constructor and calculateHash func

--- Blockchain.js
+++ Blockchain.js
@@ -1,8 +1,23 @@
 const SHA256 = require('crypto-js/sha256');
+
 class Transaction{
     constructor(fromAddress, toAddress, amount){
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+}
+
+class Block {
+    constructor(timestamp, transactions, previousHash='') {
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.timestamp + this.previousHash + JSON.stringify(this.transactions) + this.nonce).toString();
     }
 }
